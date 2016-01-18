23bec20cb474e79dc71d6296acf6b84ac5cd5d94
ceddod <cedric.dodion@chrhautesenne.be>
2016-01-18 12:26:00 +0100

    remove useless use statement

--- Routing/Route.php
+++ Routing/Route.php
@@ -12,7 +12,6 @@ namespace Lwf\Routing;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use System\Routing\RouteCompiler;
 
 
 /**
